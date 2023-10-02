270afdf6bd0f13d75a6f56a6eb2daaaa8862b6d5
yangxin6 <2362813136@qq.com>
2023-10-02 18:08:32 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# 3D 作物变形方法
+# Corn Point Cloud Virtual Deformation Tool
 
-使用方法如 demo.py
+
+**dependence**
+```
+sudo apt-get install freeglut3-dev
+```
+
+install
+
+python 3.7 +
+
+```bash
+pip install Deformation3D-1.5.2
+#pip install Deformation3D-1.5.2-py3-none-any.whl
+```
+
+demo 
+
+```bash
+python deme.py
+```
--- demo.py
+++ demo.py
@@ -7,52 +7,57 @@ from Deformation3D import prepare_vega_files, deform_models, get_models_pcd
 # Deformation3D.hello()
 
 
-config_filepath = "../configs/corn_vox.configs"
-txt_filepath = "./XY335-7-5-8.txt"
-vega_dir = "veg/"
-out_dir = "txt/"
+config_filepath = "./corn_vox.configs"
+txt_filepath = "demo-7.txt"
+vega_dir = "veg_dir/"
+out_dir = "out_dir/"
+
 leaf_num = 7
 new_num = 10
-integrator_times = 20
+integrator_times = 10
+stem_integrator_times = 7
+
 
 min_base_force = np.array([
-    [-8, -8, -200],  # stem
-    [-800, -800, -800],  # leaf 1
-    [-800, -800, -800],  # leaf 2
-    [-800, -800, -800],  # leaf 3
-    [-800, -800, -800],  # leaf 4
-    [-800, -800, -800],  # leaf 5
-    [-800, -800, -800],  # leaf 6
-    [-800, -800, -800],  # leaf 7
-    [-800, -800, -800],  # leaf 8
-    [-800, -800, -800],  # leaf 9
+    [-5, -5, -20],  # stem
+    [-5, -5, -15],  # leaf 1
+    [-5, -5, -15],  # leaf 2
+    [-5, -5, -15],  # leaf 3
+    [-5, -5, -15],  # leaf 4
+    [-5, -5, -15],  # leaf 5
+    [-5, -5, -15],  # leaf 6
+    [-5, -5, -5],   # leaf 7
 ])
 
 max_base_force = np.array([
-    [8, 8, -10],  # stem
-    [800, 800, 800],  # leaf 1
-    [800, 800, 800],  # leaf 2
-    [800, 800, 800],  # leaf 3
-    [800, 800, 800],  # leaf 4
-    [800, 800, 800],  # leaf 5
-    [800, 800, 800],  # leaf 6
-    [800, 800, 800],  # leaf 7
-    [800, 800, 800],  # leaf 8
-    [800, 800, 800],  # leaf 9
+    [5, 5, 0],  # stem
+    [5, 5, 15],  # leaf 1
+    [5, 5, 15],  # leaf 2
+    [5, 5, 15],  # leaf 3
+    [5, 5, 15],  # leaf 4
+    [5, 5, 15],  # leaf 5
+    [5, 5, 15],  # leaf 6
+    [5, 5, 5],   # leaf 7
 ])
 
 TMP_DIR = 'tmp'
 os.makedirs(TMP_DIR, exist_ok=True)
+os.makedirs(vega_dir, exist_ok=True)
+os.makedirs(out_dir, exist_ok=True)
 
 # 生成 vega文件 只用生成一次
-prepare_vega_files(txt_filepath, vega_dir, leaf_num, voxel_size=0.02)
+prepare_vega_files(txt_filepath, vega_dir, leaf_num, voxel_size=0.02, MATERIALS="\n*MATERIAL STEM\nENU, 100, 100000, 0.01\n\n*MATERIAL LEAF\nENU, 100, 1000000, 0.45\n")
 
+
+print("prepare_vega_files done ")
 # 开始变形  （可以开启多线程）
 deform_models(config_filepath, txt_filepath, vega_dir, out_dir, leaf_num, new_num,
-              min_base_force, max_base_force, integrator_times=integrator_times)
+              min_base_force, max_base_force,
+              integrator_times=integrator_times, stem_integrator_times=stem_integrator_times)
 
-# 从 obj 顶点文件种恢复 点云（可以开启多线程）
-get_models_pcd(out_dir, vega_dir, transform_axis=True, normal=False)
+print("deform_models done ")
+# 从 obj 顶点文件种恢复 点云
+get_models_pcd(out_dir, vega_dir, remove_outlier=True, transform_axis=True, normal=False)
 
 
 if os.path.exists(TMP_DIR):
